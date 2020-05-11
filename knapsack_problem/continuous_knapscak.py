39fff460907bdf6b00e401b6ce8b54e844f47e57
nenian <nenian.charles@gmail.com>
2020-05-11 14:30:28 -0400

    added comments

--- knapsack_problem/continuous_knapscak.py
+++ knapsack_problem/continuous_knapscak.py
@@ -1,16 +1,19 @@
+# Toy continuous knapsack problem
+
 class Valuables:
     def __init__(self, name, value, amount, units):
         self.name = name
         self.value = value
-        self.amount = amount
         self.units = units
-    def convert_amount_to_grams(self):
+        self.amount = self.convert_amount_to_grams(amount)
+
+    def convert_amount_to_grams(self, amount):
         if self.units == 'kg':
-            return self.amount * 1000
+            return amount * 1000
         elif self.units == 'lb':
-            return self.amount * 453.592
+            return amount * 453.592
         elif self.units == 'g':
-            return self.amount
+            return amount
 
     def get_name(self):
         return self.name 
@@ -19,15 +22,18 @@ class Valuables:
         return self.value
 
     def get_amount(self):
-        return self.convert_amount_to_grams()
+        return self.amount
     
     def get_units(self):
-        return 'all weights are processed in grams original units were ' + str(self.units)
+        return 'all weights are processed in grams original units were '\
+             + str(self.units)
     
     def set_amount_left(self, taken):
         self.amount = self.amount - taken
 
 def build_raid(items, item_value, amount_found, units):
+    # initializes Valuables 
+    # returns list of Valuables instances
     steal = []
     for i in range(len(items)):
         steal.append(Valuables(name=items[i], 
@@ -36,21 +42,46 @@ def build_raid(items, item_value, amount_found, units):
                             units=units[i]))
     return steal
 
-def greedy(steal, MaxCapacity):
-    sorted_items = sorted(steal, key = Valuables.get_value(), reversed=True)
+def greedy(steal, max_capacity):
+    # greedy routine 
+    # returns tuple of list items taken by weight and total value
+    sorted_items = sorted(steal, key = Valuables.get_value, reverse=True)
 
     hull = []
     total_value = 0.0
-    for i in range(len(sorted_items)):
+    capacity = max_capacity
+    i = 0
+    while capacity > 0:
+        if sorted_items[i].get_amount() <= capacity:
+            total_value += sorted_items[i].get_amount() * sorted_items[i].get_value()
+            hull.append((sorted_items[i].get_name(), sorted_items[i].get_amount()))
+            capacity -= sorted_items[i].get_amount()
+        else:
+            total_value += sorted_items[i].get_value() * capacity
+            hull.append((sorted_items[i].get_name(), capacity))
+            sorted_items[i].set_amount_left(capacity)
+        
+            capacity -= capacity
+        i += 1
+    
+    return (hull, total_value)
 
+def main():
+    # list indicates which items are available to steel
+    items = ['gold', 'silver', 'raisins', 'books']
+    # value of items
+    values = [53.27,0.49,0.01, 0.5]
+    # how much (weight) of each item
+    amount = [2, 5000, 15, 2] 
+    # units of mass... 
+    units = ['kg', 'g', 'lb', 'kg']
 
-items = ['gold', 'silver', 'raisins']
-values = [53.27,0.49,0.01]
-amount = [2, 5000, 15] 
-units = ['kg', 'g', 'lb']
+    # initalize instance that catalogs items available
+    steal_items = build_raid(items, values, amount, units)
 
-steal_items = build_raid(items, values, amount, units)
+    # execute greedy routine to fill 
+    # knapsack of specified max capacity
+    print(greedy(steal=steal_items, max_capacity=5000))
 
-
-
-print(steal_items[0].get_units())
+#run
+main()
